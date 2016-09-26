c19e58de7f2b77dca5854ccf34fbc2e1eb89a1ea
Sam Burnstone <sburnstone@scottlogic.com>
2016-09-26 09:14:08 +0100

    Base single application project

---
+++ 08 - Live Photo Extensions/LivePhotoWatermarker/LivePhotoWatermarker/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  LivePhotoWatermarker
+//
+//  Created by Samuel Burnstone on 26/09/2016.
+//  Copyright © 2016 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ 08 - Live Photo Extensions/LivePhotoWatermarker/LivePhotoWatermarker/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  LivePhotoWatermarker
+//
+//  Created by Samuel Burnstone on 26/09/2016.
+//  Copyright © 2016 ShinobiControls. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+}
+
