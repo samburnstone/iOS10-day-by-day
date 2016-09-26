e43bcaf17602fac60e3d2169d62798b4b072daad
Sam Burnstone <sburnstone@scottlogic.com>
2016-09-26 17:19:01 +0100

    WIP: Move from watermarking to blurring image

--- 08 - Live Photo Extensions/LivePhotoWatermarker/WatermarkerExtension/PhotoEditingViewController.swift
+++ 08 - Live Photo Extensions/LivePhotoWatermarker/WatermarkerExtension/PhotoEditingViewController.swift
@@ -16,9 +16,15 @@ class PhotoEditingViewController: UIViewController, PHContentEditingController {
     
     @IBOutlet weak var photoView: PHLivePhotoView!
     
+    lazy var livePhotoContext: PHLivePhotoEditingContext = {
+        return PHLivePhotoEditingContext(livePhotoEditingInput: self.input!)!
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        photoView.contentMode = .scaleAspectFit
     }
     
     override func didReceiveMemoryWarning() {
@@ -44,26 +50,25 @@ class PhotoEditingViewController: UIViewController, PHContentEditingController {
         }
         
         input = contentEditingInput
+        
+        // Show alert
+        requestWatermarkDetails()
     }
     
     func finishContentEditing(completionHandler: @escaping ((PHContentEditingOutput?) -> Void)) {
-        // Update UI to reflect that editing has finished and output is being rendered.
         
-        // Render and provide output on a background queue.
-        DispatchQueue.global().async {
-            // Create editing output from the editing input.
-            let output = PHContentEditingOutput(contentEditingInput: self.input!)
-            
-            // Provide new adjustments and render output to given location.
-            // output.adjustmentData = <#new adjustment data#>
-            // let renderedJPEGData = <#output JPEG#>
-            // renderedJPEGData.writeToURL(output.renderedContentURL, atomically: true)
-            
-            // Call completion handler to commit edit to Photos.
-            completionHandler(output)
-            
-            // Clean up temporary files, etc.
+        let output = PHContentEditingOutput(contentEditingInput: self.input!)
+
+        livePhotoContext.saveLivePhoto(to: output) {
+            success, error in
+            if success {
+                completionHandler(output)
+            }
+            else {
+                print("There was a problem saving the photo :(")
+            }
         }
+        
     }
     
     var shouldShowCancelConfirmation: Bool {
@@ -76,5 +81,106 @@ class PhotoEditingViewController: UIViewController, PHContentEditingController {
         // Clean up temporary files, etc.
         // May be called after finishContentEditingWithCompletionHandler: while you prepare output.
     }
+}
 
+extension PhotoEditingViewController {
+    
+    fileprivate func updateLivePhotoView() {
+        let size = input!.displaySizeImage!.size
+        livePhotoContext.prepareLivePhotoForPlayback(withTargetSize: size, options: nil) {
+            [weak self]
+            livePhoto, error in
+            self?.photoView.livePhoto = livePhoto
+        }
+
+    }
+}
+
+extension PhotoEditingViewController {
+    
+    /// Brings up UI to allow the user to input the text they wish to watermark their photo with.
+    fileprivate func requestWatermarkDetails() {
+        
+        let alert = UIAlertController(title: "Watermark Title", message: "What would like to scrawl over your lovely photo?", preferredStyle: .alert)
+        
+        alert.addTextField(configurationHandler: {
+            textfield in
+            textfield.placeholder = "John Appleseed"
+        })
+        
+        // On cancellation, simply dimiss the extension
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+        
+        let watermarkAction = UIAlertAction(title: "Add", style: .default) {
+            [weak self]
+            _ in
+            guard let watermarkText = alert.textFields?[0].text else { return }
+            
+            let prefixedWatermarkText = "© \(watermarkText)"
+            
+            self?.applyWatermark(with: prefixedWatermarkText)
+            self?.updateLivePhotoView()
+        }
+        alert.addAction(watermarkAction)
+        
+        present(alert, animated: true)
+    }
+
+    
+    fileprivate func applyWatermark(with title: String) {
+        guard let image = UIImage.image(from: title, with: photoView.bounds.size)
+            else {
+                fatalError("Could not create image")
+        }
+
+        let watermarkImage = CIImage(cgImage: image.cgImage!)
+        
+        livePhotoContext.frameProcessor = {
+            frame, _ in
+            
+            return frame.image.applyingFilter("CIGaussianBlur", withInputParameters: [kCIInputRadiusKey: 10])
+        }
+    }
+}
+
+extension UIImage {
+    
+    /// Creates an image with the given text string.
+    static func image(from text: String, with size: CGSize) -> UIImage? {
+        
+        // To cater for the LivePhoto zooming, we need to add a bit of padding between the text and the edge of the screen
+        let insets = UIEdgeInsets(top: 0, left: 20, bottom: 20, right: 0)
+        
+        let font = UIFont(name: "Marker Felt", size: 40)!
+        
+        // Draw the text in an an image context and return it
+        UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
+        
+        let imageRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        
+        let image = UIImage()
+        image.draw(in: imageRect)
+        
+        let convertedString = text as NSString
+        
+        let attributes = [
+            NSFontAttributeName : font,
+            NSForegroundColorAttributeName : UIColor.white
+        ]
+        
+        let textSize = convertedString.size(attributes: attributes)
+        let textRect = CGRect(x: insets.left,
+                              y: (size.height - insets.bottom - textSize.height),
+                              width: textSize.width,
+                              height: textSize.height)
+        
+        convertedString.draw(in: textRect, withAttributes: attributes)
+        
+        let textImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        return textImage
+    }
 }
